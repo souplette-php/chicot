f2bd2547a02e0eed03a35d4c3392cfba16747551
ju1ius <jules.bernable@gmail.com>
2024-01-10 16:36:23 +0100

    chore: bumps cli version

--- bin/chicot.php
+++ bin/chicot.php
@@ -6,7 +6,7 @@ use Souplette\Chicot\Command\GenerateStubsCommand;
 use Souplette\Chicot\Command\ListModulesCommand;
 use Symfony\Component\Console\Application;
 
-$app = new Application('chicot', '0.1.2');
+$app = new Application('chicot', '0.1.4');
 $app->add(new ListModulesCommand('modules'));
 $app->add(new GenerateStubsCommand('stub'));
 $app->run();
